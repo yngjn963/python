1713bef4974323a48d73842510796299cc50700b
yngjn963 <yngjn963@naver.com>
2023-09-12 21:34:25 +0900

    feat: 파이썬
    섹션 1. 자료형
    boolean 자료형

--- junior/.vscode/practice.py
+++ junior/.vscode/practice.py
@@ -10,3 +10,12 @@ print('풍선')
 print("나비")
 print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
 print("ㅋ"*10)
+
+# 참 / 거짓
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
