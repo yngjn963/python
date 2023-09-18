75c1312f4486d61d4582eabf03950475bfef3b9f
yngjn963 <yngjn963@naver.com>
2023-09-18 23:46:45 +0900

    feat: 파이썬
    섹션 2. 연산자
    숫자 처리 함수

--- junior/.vscode/practice.py
+++ junior/.vscode/practice.py
@@ -93,3 +93,15 @@ print(number)
 #number %= 2 # 0
 number %= 5 # 1
 print(number)
+
+print(abs(-5)) # 절대값 5
+print(pow(4, 2)) # 4^2 = 4*4 = 16
+print(max(5, 12)) # 12
+print(min(5, 12)) # 5
+print(round(3.14)) # 3
+print(round(4.99)) # 5
+
+from math import * # math 라이브러리 안의 모든 것 이용
+print(floor(4.99)) # 내림 4
+print(ceil(3.14)) # 올림 4
+print(sqrt(16)) # 제곱근 4
