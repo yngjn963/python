5c55809f9fc11c8c00832124ee18d573eccfb739
yngjn963_lt1 <yngjn963@naver.com>
2023-10-10 22:24:07 +0900

    feat: 파이썬
    섹션 3. 문자열 처리
    슬라이싱

--- junior/.vscode/practice.py
+++ junior/.vscode/practice.py
@@ -138,3 +138,14 @@ sentence3 = """
 파이썬은 쉬워요
 """ # 여러 줄의 문자열
 print(sentence3)
+
+jumin = "990120-1234567"
+print("성별: " + jumin[7])
+print("년생: " + jumin[0:2]) # 인덱스 0 부터 2 전까지(0, 1)
+print("월: " + jumin[2:4])
+print("일: " + jumin[4:6])
+# print("생년월일: " + jumin[0:6])
+print("생년월일: " + jumin[:6]) # 인덱스 처음부터 6 전까지
+# print("뒤 7자리: " + jumin[7:14])
+print("뒤 7자리: " + jumin[7:]) # 인덱스 7 부터 끝까지
+print("뒤 7자리(뒤에서부터): " + jumin[-7:]) # 인덱스 맨 뒤에서 7번째부터 끝까지
