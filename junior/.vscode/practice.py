8d69b539539e427d65c6756e234554f992021d54
yngjn963_lt1 <yngjn963@naver.com>
2023-10-11 22:16:47 +0900

    feat: 파이썬
    섹션 3. 문자열 처리
    문자열 포맷

--- junior/.vscode/practice.py
+++ junior/.vscode/practice.py
@@ -164,3 +164,27 @@ print(python.find("Python"))
 print(python.find("Java")) # -1
 # print(python.index("Python")) # ValueError. 이후 프로그램을 실행하지 않고 종료.
 print(python.count("n")) # n 개수
+
+print("a" + "b")
+print("a", "b")
+# 방법 1
+print("나는 %d살입니다." % 20) # d: 정수
+print("나는 %s을 좋아해요." % "파이썬") # s: 문자열
+print("Apple은 %c로 시작해요." % "A") # c: 문자
+print("나는 %s살입니다." % 20) # s: 정수, 문자열, 문자 모두 가능
+print("나는 %s색과 %s색을 좋아해요." % ("파란", "빨간"))
+
+# 방법 2
+print("나는 {}살입니다.".format(20))
+print("나는 {}색과 {}색을 좋아해요.".format("파란", "빨간"))
+print("나는 {0}색과 {1}색을 좋아해요.".format("파란", "빨간"))
+print("나는 {1}색과 {0}색을 좋아해요.".format("파란", "빨간"))
+
+# 방법 3
+print("나는 {age}살이며, {color}색을 좋아해요.".format(age = 20, color = "빨간"))
+print("나는 {age}살이며, {color}색을 좋아해요.".format(color = "빨간", age = 20))
+
+# 방법 4(파이썬 버전 3.6 이상)
+age = 20
+color = "빨간"
+print(f"나는 {age}살이며, {color}색을 좋아해요.")
