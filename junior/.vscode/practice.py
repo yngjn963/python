1b68d80fd1f85ea31f45b6250914a0e4665aa0b3
yngjn963_lt1 <yngjn963@naver.com>
2023-10-11 22:25:18 +0900

    feat: 파이썬
    섹션 3. 문자열 처리
    탈출문자

--- junior/.vscode/practice.py
+++ junior/.vscode/practice.py
@@ -188,3 +188,27 @@ print("나는 {age}살이며, {color}색을 좋아해요.".format(color = "빨�
 age = 20
 color = "빨간"
 print(f"나는 {age}살이며, {color}색을 좋아해요.")
+
+# 탈출문자
+# \n: 줄바꿈
+# print("백문이 불여일견
+#        백견이 불여일타")
+print("백문이 불여일견\n백견이 불여일타")
+
+# \", \': 문장 내에서 따옴표
+# print("저는 "나도코딩"입니다.")
+print('저는 "나도코딩"입니다.')
+print("저는 \"나도코딩\"입니다.")
+
+# \\: 문장 내에서 \
+# print("C:\Python311\python.exe")
+print("C:\\Python311\\python.exe")
+
+# \r: 커서를 맨 앞으로 이동
+print("Red Apple\rPine") # PineApple
+
+# \b: 백 스페이스
+print("Redd\bApple") # RedApple
+
+# \t: 탭
+print("Red\tApple") # Red     Apple
