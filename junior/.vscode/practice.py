a7b01110ce8a59fc6a4a126e4c7b5a13ca6abef9
yngjn963_lt1 <yngjn963@naver.com>
2023-10-14 22:55:31 +0900

    feat: 파이썬
    섹션 4. 자료구조
    리스트

--- junior/.vscode/practice.py
+++ junior/.vscode/practice.py
@@ -234,3 +234,60 @@ site = "http://naver.com"
 # print(site[site.index("http://") + 7:site.index(".com")][0:3])
 pw = site[site.index("http://") + 7:site.index(".com")] # .com이 아닌 다른 사이트 경우를 생각하면 수정 필요
 print(f"생성된 비밀번호: {pw[0:3]}{len(pw)}{pw.count('e')}!")
+
+# 리스트: 순서를 가지는 집합 객체
+# 지하철 칸별로 10명, 20명, 30명
+subway1 = 10
+subway2 = 20
+subway3 = 30
+
+subway = [10, 20, 30]
+print(subway)
+
+subway = ["유재석", "조세호", "박명수"]
+print(subway)
+# 조세호는 몇 번째 칸에 타고 있는가?
+print(subway.index("조세호"))
+
+# 다음 정류장에서 하하가 다음 칸에 탐
+subway.append("하하")
+print(subway)
+
+# 정형돈이 유재석과 조세호 사이 칸에 탐
+subway.insert(1, "정형돈")
+print(subway)
+
+# 마지막 칸부터 내림
+print(subway.pop()) # 내린 사람
+print(subway) # 내리고 지하철에 남은 사람
+# print(subway.pop()) # 내린 사람
+# print(subway) # 내리고 지하철에 남은 사람
+# print(subway.pop()) # 내린 사람
+# print(subway) # 내리고 지하철에 남은 사람
+
+# 같은 이름의 사람이 몇 명 있는지 확인하기
+subway.append("유재석")
+print(subway)
+print(subway.count("유재석"))
+
+# 정렬하기
+num_list = [5,2,4,3,1]
+num_list.sort()
+print(num_list)
+
+# 역으로 정렬하기
+num_list.reverse()
+print(num_list)
+
+# 모두 지우기
+num_list.clear()
+print(num_list)
+
+# 서로 다른 자료형을 함께 사용
+mix_list = ["조세호", 20, True]
+print(mix_list)
+
+# 리스트 확장(리스트 합치기)
+num_list = [5,2,4,3,1]
+num_list.extend(mix_list)
+print(num_list)
