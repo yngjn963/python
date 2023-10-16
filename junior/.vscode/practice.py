0625de3e1475bf16f1ec5dda797fdb6d625dcaf1
yngjn963 <yngjn963@naver.com>
2023-10-16 22:18:46 +0900

    feat: 파이썬
    섹션 4. 자료구조
    세트

--- junior/.vscode/practice.py
+++ junior/.vscode/practice.py
@@ -330,6 +330,7 @@ print(cabinet.items()) # dict_items([('B-100', '김태호'), ('C-20', '조세호
 cabinet.clear()
 print(cabinet) # {}
 
+
 # 튜플: 변경할 수 없지만 성능이 좋다.
 menu = ("돈까스", "치즈까스")
 print(menu[0])
@@ -344,3 +345,32 @@ print(name, age, hobby)
 
 (name, age, hobby) = ("김종국", 20, "코딩") # 서로 다른 변수를 한 번에 선언
 print(name, age, hobby)
+
+
+# 집합(Set)
+# 중복 안됨, 순서 없음
+my_set = {1,2,3,3,3}
+print(my_set) # {1,2,3}
+
+java = {"유재석", "김태호", "양세형"}
+python = set(["유재석", "박명수"])
+
+# 교집합(java와 python을 모두 할 수 있는 개발자)
+print(java & python)
+print(java.intersection(python))
+
+# 합집합(java를 할 수 있거나 python을 할 수 있는 개발자)
+print(java | python)
+print(java.union(python)) # {'김태호', '양세형', '유재석', '박명수'} - 순서 상관없음
+
+# 차집합(java는 할 수 있지만 python은 할 줄 모르는 개발자)
+print(java - python)
+print(java.difference(python))
+
+# python을 할 줄 아는 개발자가 늘어남
+python.add("김태호")
+print(python)
+
+# java를 까먹음
+java.remove("김태호")
+print(java)
