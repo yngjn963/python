581a7258d64c1a8dd816b893bf841d355e00a5c7
yngjn963 <yngjn963@naver.com>
2023-10-25 21:44:34 +0900

    feat: 파이썬
    섹션 5. 제어문
    if

--- junior/.vscode/practice.py
+++ junior/.vscode/practice.py
@@ -436,3 +436,23 @@ print("-- 당첨자 발표 --")
 print("치킨 당첨자: {0}".format(winners[0]))
 print("커피 당첨자: {0}".format(winners[1:]))
 print("-- 축하합니다 --")
+
+# weather = input("오늘 날씨는 어때요? ")
+# # if 조건:
+# #     실행 명령문
+# if weather == "비" or weather == "눈":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지":
+#     print("마스크를 챙기세요")
+# else:
+#     print("준비물 필요 없어요")
+
+temp = int(input("기온은 어때요? "))
+if temp >= 30:
+    print("너무 더워요. 나가지 마세요")
+elif temp < 30 and temp >= 10:
+    print("괜찮은 날씨에요")
+elif 0 <= temp < 10:
+    print("외투를 챙기세요")
+else:
+    print("너무 추워요. 나가지 마세요")
