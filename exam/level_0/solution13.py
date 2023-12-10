f5598db39636acec1fb3fc9157eaad91e51fd0b0
yngjn963 <yngjn963@naver.com>
2023-12-10 10:55:14 +0900

    feat: 프로그래머스 코딩테스트 연습
    13

---
+++ exam/level_0/solution13.py
@@ -0,0 +1,25 @@
+"""
+최빈값 구하기
+
+최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다. 정수 배열 array 가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요. 최빈값이 여러 개면 -1 을 return 합니다.
+
+제한사항
+ - 0 < array 의 길이 < 100
+ - 0 <= array 의 원소 < 1,000
+
+입출력 예
+array = [1, 2, 3, 3, 3, 4]
+result = 3
+
+array = [1, 1, 2, 2]
+result = -1
+
+array = [1]
+result = 1
+"""
+
+def solution(array):
+    answer = 0
+    return answer
+
+solution([1, 2, 3, 3, 3, 4])
